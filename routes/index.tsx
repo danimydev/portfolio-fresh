94c2c9545428efef5683e1d5515bfe5b6adb5c36
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-03-21 18:54:01 -0500

    adds title and meta tags tp index

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,22 @@
 import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import { PageProps } from "$fresh/server.ts";
 
-export default function Home() {
+const TITLE = "@danimydev";
+const DESCRIPTION =
+  "Daniel Madrid portfolio website built with Fresh JS / Deno";
+
+export default function Home(props: PageProps) {
   return (
     <>
       <Head>
-        <title>@danimydev</title>
+        <title>{TITLE}</title>
+        <meta name="description" content={DESCRIPTION} />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={props.url.href} />
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <Counter start={3} />
-      </div>
+      Comming soon...
     </>
   );
 }
