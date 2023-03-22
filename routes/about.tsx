c2d3cba5a5099a5dc97321f99bcf0c9300761074
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-03-21 23:32:11 -0500

    about page

---
+++ routes/about.tsx
@@ -0,0 +1,32 @@
+import { Head } from "$fresh/runtime.ts";
+import { PageProps } from "$fresh/server.ts";
+import Header from "./components/Header.tsx";
+
+const TITLE = "@danimydev";
+const DESCRIPTION =
+  "Daniel Madrid portfolio website built with Fresh JS / Deno";
+
+export default function Home(props: PageProps) {
+  return (
+    <>
+      <Head>
+        <title>{TITLE}</title>
+        <meta name="description" content={DESCRIPTION} />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={props.url.href} />
+      </Head>
+
+      <body class="md:container md:mx-auto">
+        <Header active="/about" />
+
+        <main>
+        </main>
+
+        <footer>
+        </footer>
+      </body>
+    </>
+  );
+}
