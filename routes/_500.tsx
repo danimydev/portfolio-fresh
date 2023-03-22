a4fdf38aa3f6f17d71ed0ea79b3ab5c5fba158fb
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-03-21 23:31:50 -0500

    custom 500 page

--- routes/_500.tsx
+++ routes/_500.tsx
@@ -1,5 +1,15 @@
 import { ErrorPageProps } from "$fresh/server.ts";
 
 export default function Error500Page({ error }: ErrorPageProps) {
-  return <p>500 internal error: {(error as Error).message}</p>;
+  return (
+    <main class="h-screen w-full flex flex-col justify-center items-center">
+      <h1 class="text-9xl font-extrabold tracking-widest">500</h1>
+      <div class="bg-[#2eff7b] px-2 text-sm rounded rotate-12 absolute">
+        Server Error
+      </div>
+      <button class="mt-5">
+        <a class="hover:underline" href="/">Go Home</a>
+      </button>
+    </main>
+  );
 }
