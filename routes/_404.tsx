d276fb81fad68ec960100cbcda03a83ca9809a34
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-03-21 23:31:44 -0500

    custom 404 page

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,5 +1,15 @@
 import { UnknownPageProps } from "$fresh/server.ts";
 
 export default function NotFoundPage({ url }: UnknownPageProps) {
-  return <p>404 not found: {url.pathname}</p>;
+  return (
+    <main class="h-screen w-full flex flex-col justify-center items-center">
+      <h1 class="text-9xl font-extrabold tracking-widest">404</h1>
+      <div class="bg-[#2eff7b] px-2 text-sm rounded rotate-12 absolute">
+        Page Not Found
+      </div>
+      <button class="mt-5">
+        <a class="hover:underline" href="/">Go Home</a>
+      </button>
+    </main>
+  );
 }
