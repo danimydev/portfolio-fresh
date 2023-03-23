cd6e545dc13c97baec7293eedf27e56f444d69b2
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-03-22 23:21:28 -0500

    move Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import NavigationBar from "./NavigationBar.tsx";
 export default function Header(props: { active: string }) {
   return (
     <div>
-      <header class="mx-auto max-w-screen-lg flex gap-3">
-        <NavigationBar class="hidden md:flex" active={props.active} />
+      <header>
+        <NavigationBar active={props.active} />
       </header>
     </div>
   );
