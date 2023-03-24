d98acc70b3b7f86d4b3cb63ef023ff715e78c561
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-03-23 22:31:30 -0500

    adds github profile link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,11 @@
 export default function Header() {
   return (
     <div>
-      <footer class="text-center text-sm">
-        Made with &#10084; @danimydev
+      <footer class="text-center text-sm py-6">
+        Made with &#10084;{" by "}
+        <a class="font-medium" href="https://www.github.com/danimydev">
+          @danimydev
+        </a>
       </footer>
     </div>
   );
