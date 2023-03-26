2ebaede1d8750c7f7dd5291729c06bf505a885a0
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-03-26 01:42:53 -0500

    add biography component

---
+++ components/Biography.tsx
@@ -0,0 +1,14 @@
+export default function Biography() {
+  return (
+    <div class="italic my-4">
+      <p class="my-3">Hey👋,</p>
+      <p class="my-3">I am Daniel Madrid,</p>
+      <p class="my-3">
+        I was born in Colombia, I studied System Engineer at Universidad del
+        Norte in Barranquilla. Currently I work at CI&T as a{" "}
+        <strong>Backend Developer</strong>, I am part of a team which builds
+        digital products and also maintain multiple IT services!
+      </p>
+    </div>
+  );
+}
