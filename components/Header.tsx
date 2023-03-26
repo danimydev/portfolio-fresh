1e22a2b8207ed7a89d71566659e4f6648e658814
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-03-26 01:43:28 -0500

    fix padding on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import NavigationBar from "./NavigationBar.tsx";
 
 export default function Header(props: { active: string }) {
   return (
-    <header class="py-6 flex flex-col md:flex-row gap-4 items-center">
-      <div class="">
+    <header class="py-10 flex flex-col md:flex-row gap-4 items-center">
+      <div>
         <img
           class="w-20"
           src="/logo_animated.gif"
