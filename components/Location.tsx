1a87910d35dc3aeb7fa2f7bbfefc43e433f4a17a
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-03-25 21:04:45 -0500

    adds Location component to about page

---
+++ components/Location.tsx
@@ -0,0 +1,42 @@
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export default function Location() {
+  function date() {
+    const date = new Date();
+    return `(${monthNames[date.getMonth()]} ${date.getFullYear()})`;
+  }
+
+  return (
+    <div class="flex p-5 border-1">
+      <div>
+        📍
+      </div>
+      <div>
+        <div class="opacity-50">
+          {date()}
+        </div>
+        <div class="flex items-center">
+          Barranquilla, Colombia &#x1F1E8;&#x1F1F4;
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Colombia.svg"
+            alt="colombia flag"
+            class="w-4 h-3 ml-2 rounded"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- routes/about.tsx
+++ routes/about.tsx
@@ -2,6 +2,7 @@ import { Head } from "$fresh/runtime.ts";
 import { PageProps } from "$fresh/server.ts";
 import Footer from "../components/Footer.tsx";
 import Header from "../components/Header.tsx";
+import Location from "../components/Location.tsx";
 
 const TITLE = "@danimydev";
 const DESCRIPTION =
@@ -21,6 +22,18 @@ export default function Home(props: PageProps) {
 
       <body class="max-w-xl mx-auto font-mono">
         <Header active="/about"></Header>
+        <div class="pb-4">
+          <h3 class="font-bold">About Me</h3>
+          <p class="italic my-4">
+            Hey👋,
+            <br />
+            I was born in Colombia, I studied System Engineer at Universidad del
+            Norte in Barranquilla. Currently I work at CI&T as a{" "}
+            <strong>Backend Developer</strong>, I am part of a team which builds
+            digital products and also maintain multiple IT services!
+          </p>
+        </div>
+        <Location></Location>
         <Footer></Footer>
       </body>
     </>
