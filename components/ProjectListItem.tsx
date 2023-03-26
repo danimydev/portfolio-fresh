52047dc3715d42c002fed248672aeb23ffbe0af6
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-03-26 01:44:29 -0500

    rm useless div

--- components/ProjectListItem.tsx
+++ components/ProjectListItem.tsx
@@ -5,23 +5,21 @@ export default function ProjectListItem(props: { repo: GitHubRepo }) {
   return (
     <div class="md:container my-4">
       <div>
-        <div>
-          <a href={props.repo.html_url} target="_blank" class="text-underline">
-            {props.repo.name}:
-          </a>{" "}
-          <span>{props.repo.description}</span>
-        </div>
+        <a href={props.repo.html_url} target="_blank" class="text-underline">
+          {props.repo.name}:
+        </a>{" "}
+        <span>{props.repo.description}</span>
+      </div>
 
-        <div class="flex">
-          <div
-            class={`mr-2 px-2 py-2 rounded ${
-              LANGUAGE_CLASSES.get(props.repo.language.toLowerCase())
-            }`}
-          >
-          </div>
-          <div class="text-xs">
-            {props.repo.language}
-          </div>
+      <div class="flex">
+        <div
+          class={`mr-2 px-2 py-2 rounded ${
+            LANGUAGE_CLASSES.get(props.repo.language.toLowerCase())
+          }`}
+        >
+        </div>
+        <div class="text-xs">
+          {props.repo.language}
         </div>
       </div>
     </div>
