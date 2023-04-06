bd95ffca1e2f7da58645a6cdb0d248e951e7fde4
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-04-05 20:52:20 -0500

    delete contact component

--- routes/about.tsx
+++ routes/about.tsx
@@ -5,7 +5,6 @@ import Header from "../components/Header.tsx";
 import Location from "../components/Location.tsx";
 import Biography from "../components/Biography.tsx";
 import Experience from "../components/Experience.tsx";
-import Contact from "../components/Contact.tsx";
 
 const TITLE = "@danimydev";
 const DESCRIPTION =
@@ -33,7 +32,6 @@ export default function Home(props: PageProps) {
         <Biography></Biography>
         <Location></Location>
         <Experience></Experience>
-        <Contact></Contact>
         <Footer></Footer>
       </body>
     </>
